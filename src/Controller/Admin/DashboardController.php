0eee9f1dd057092bb3c6331416dccb3365a7ce4f
ghazaloran <ghazaloran2@gmail.com>
2019-12-08 14:43:05 +0100

    'INITIAL_COMMIT'

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin_dashboard")
+     */
+    public function index()
+    {
+
+        return $this->render('admin/dashboard.html.twig', []);
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Artist;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -13,6 +14,7 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(EntityManagerInterface $em)
     {
